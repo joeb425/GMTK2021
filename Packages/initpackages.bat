c574db1cb49df429ce2220845b3f9653be451d9e
fpwong <func.pwong@gmail.com>
2021-12-08 18:53:17 +1100

    Add init packages batch file

---
+++ Packages/initpackages.bat
@@ -0,0 +1,10 @@
+git clone https://github.com/mantisdevs/mantis.attributesystem.git
+git clone https://github.com/mantisdevs/mantis.engine.git
+git clone https://github.com/mantisdevs/mantis.gameplaytags.git
+git clone https://github.com/mantisdevs/mantis.hex.git
+git clone https://github.com/mantisdevs/mantis.utils.git
+git -C ./mantis.attributesystem pull
+git -C ./mantis.engine pull
+git -C ./mantis.gameplaytags pull
+git -C ./mantis.hex pull
+git -C ./mantis.utils pull
