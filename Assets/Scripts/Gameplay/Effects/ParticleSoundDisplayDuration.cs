62f54e0bf354ad40474af29ef9a34e1c6a02d4f4
fpwong <func.pwong@gmail.com>
2021-12-12 16:07:44 +1100

    Added visual display for effects

---
+++ Assets/Scripts/Gameplay/Effects/ParticleSoundDisplayDuration.cs
@@ -0,0 +1,31 @@
+﻿using Mantis.AttributeSystem;
+using UnityEngine;
+
+namespace DefaultNamespace.Enemy
+{
+	[CreateAssetMenu(menuName = "Gameplay/Effects/ParticleSoundDisplayInstant")]
+	public class ParticleSoundDisplayDuration : EffectDisplay
+	{
+		private ParticleSystem ps;
+
+		public override void OnApplied(GameplayAttributeContainer attributeContainer, EffectDisplayParameters parameters, EffectParameters effectParameters)
+		{
+			if (parameters.audioClip)
+			{
+				Game.Get.GetAudioHandler().PlaySfx(parameters.audioClip);
+			}
+
+			if (parameters.particleSystem)
+			{
+				ps = Instantiate(parameters.particleSystem, attributeContainer.transform);
+				ps.transform.position = attributeContainer.transform.position;
+				ps.Play();
+			}
+		}
+
+		public override void OnRemoved()
+		{
+			Destroy(ps);
+		}
+	}
+}
---
+++ Assets/Scripts/Gameplay/Effects/ParticleSoundDisplayInstant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Mantis.AttributeSystem;
+using UnityEngine;
+
+namespace DefaultNamespace.Enemy
+{
+	[CreateAssetMenu(menuName = "Gameplay/Effects/ParticleSoundDisplayInstant")]
+	public class ParticleSoundDisplayInstant : EffectDisplay
+	{
+		public override void OnApplied(GameplayAttributeContainer attributeContainer, EffectDisplayParameters parameters, EffectParameters effectParameters)
+		{
+			if (parameters.audioClip)
+			{
+				Game.Get.GetAudioHandler().PlaySfx(parameters.audioClip);
+			}
+
+			if (parameters.particleSystem)
+			{
+				ParticleSystem ps = Instantiate(parameters.particleSystem);
+				ps.transform.position = attributeContainer.transform.position;
+				ps.Play();
+			}
+		}
+
+		public override void OnRemoved()
+		{
+		}
+	}
+}
