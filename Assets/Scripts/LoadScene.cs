7871fb2e23b34c5549663a9c13e7fb486543b763
fpwong <func.pwong@gmail.com>
2022-01-03 16:28:32 +1100

    Improve batching by making stuff static. Add temp loading screen.

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -1,5 +1,9 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.Android;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
 
 namespace DefaultNamespace
 {
@@ -8,12 +12,31 @@ namespace DefaultNamespace
 		[SerializeField]
 		private AssetReference scene;
 
+		private string _percentText = String.Empty;
+
+		private AsyncOperationHandle<SceneInstance> loadOperation; 
+
 		private void Awake()
 		{
-			var op = Addressables.LoadSceneAsync(scene);
-			op.Completed += handle =>
+			loadOperation = Addressables.LoadSceneAsync(scene);
+		}
+
+		private void Update()
+		{
+			if (loadOperation.IsValid())
 			{
-			};
+				_percentText = "" + loadOperation.PercentComplete;
+				Debug.Log($"Loading scene {loadOperation.PercentComplete}");
+			}
+			else
+			{
+				_percentText = "";
+			}
+		}
+
+		void OnGUI()
+		{
+			GUI.Label(new Rect(100, 100, 200, 200), _percentText);
 		}
 	}
 }
