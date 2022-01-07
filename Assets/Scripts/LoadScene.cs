b791f156b262fa2fb65441716eb3073a3f3a7b07
fpwong <func.pwong@gmail.com>
2022-01-07 12:24:50 +1100

    Improve loading screen

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.Android;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 namespace DefaultNamespace
 {
@@ -12,25 +15,51 @@ namespace DefaultNamespace
 		[SerializeField]
 		private AssetReference scene;
 
+		[SerializeField]
+		private UIDocument loadingScreen;
+
 		private string _percentText = String.Empty;
 
-		private AsyncOperationHandle<SceneInstance> loadOperation; 
+		private AsyncOperationHandle<SceneInstance> loadOperation;
+
+		private bool _isLevelLoaded;
+		private ProgressBar _progressLabel;
+		private Label _statusLabel;
 
 		private void Awake()
 		{
-			loadOperation = Addressables.LoadSceneAsync(scene);
+			var ve = loadingScreen.rootVisualElement;
+			_progressLabel = ve.Q<ProgressBar>("ProgressBar");
+			_statusLabel = ve.Q<Label>("StatusLabel");
 		}
+
+		IEnumerator Start()
+		{
+			loadOperation = Addressables.LoadSceneAsync(scene, LoadSceneMode.Single, false);
+			yield return loadOperation;
+
+			_statusLabel.text = "Press any key...";
+		}
+		
 
 		private void Update()
 		{
 			if (loadOperation.IsValid())
 			{
+				float percent = loadOperation.PercentComplete * 100.0f;
+				string percentText = percent + "%";
 				_percentText = "" + loadOperation.PercentComplete;
-				Debug.Log($"Loading scene {loadOperation.PercentComplete}");
+				_progressLabel.value = percent;
+				_progressLabel.title = percentText;
 			}
 			else
 			{
 				_percentText = "";
+			}
+
+			if (loadOperation.IsDone && (Input.anyKeyDown || Input.touchCount > 0))
+			{
+				loadOperation.Result.ActivateAsync();
 			}
 		}
 
